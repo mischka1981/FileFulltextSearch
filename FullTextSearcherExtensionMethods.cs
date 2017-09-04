bfe11924e4f287673815556ad345c8ae7e7069fd
mischka1981 <michael.leben@kontacts.de>
2017-09-04 15:14:55 +0200

    First version

---
+++ FullTextSearcherExtensionMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileFulltextSearch {
+    public static class FullTextSearcherExtensionMethods {
+
+        /// <summary>
+        /// https://stackoverflow.com/questions/2641326/finding-all-positions-of-a-substring-in-a-large-string-in-c-sharp
+        /// from
+        /// https://stackoverflow.com/users/227267/matti-virkkunen
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static List<int> AllIndexesOf(this string str, string value) {
+            if (String.IsNullOrEmpty(value))
+                throw new ArgumentException("the string to find may not be empty", "value");
+            List<int> indexes = new List<int>();
+            for (int index = 0; ; index += value.Length) {
+                index = str.IndexOf(value, index);
+                if (index == -1)
+                    return indexes;
+                indexes.Add(index);
+            }
+        }
+
+    }
+}
